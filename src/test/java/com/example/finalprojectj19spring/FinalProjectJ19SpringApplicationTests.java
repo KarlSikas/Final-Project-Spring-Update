225e4ece420a8829a214596ed7a2e93d104c1266
KarlSikas <karlsirkas@outlook.com>
2023-05-04 18:31:50 +0300

    Final Project Update

---
+++ src/test/java/com/example/finalprojectj19spring/FinalProjectJ19SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectj19spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectJ19SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
